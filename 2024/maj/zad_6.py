022a799ed74e40ae2bbddf3b32d6e174bb86630a
Marcin Szablak <Marcin-Szablak@proton.me>
2025-02-17 14:27:45 +0100

    Finished zad 5 Finished zad 6 Started zad 7

---
+++ 2024/maj/zad_6.py
@@ -0,0 +1,23 @@
+liczba1 = int('101112', 3)
+liczba2 = int('121', 9)
+
+wynik1 = format(liczba1 + liczba2)
+wynik2 = format(liczba1 - liczba2)
+
+print(wynik1)
+print(wynik2)
+
+'''
+następnie bierzemy nasze wyniki i przypisujemy je do zmiennej w js
+y = 384
+x = 184
+
+następnie kożystając z funkcji toString konwertujemy na system dziesiętny
+
+y.toString(3)
+x.toString(3)
+
+co będzie w konsoli:
+y = 112020
+x = 20211
+'''
