b80bf1065a48d468d66ac596ca2c2707f985a875
Marcin Szablak <m.szablak30@gmail.com>
2025-02-20 21:32:03 +0100

    Finished zad 5 Finished zad 6

---
+++ 2024/grudzien/zad6.py
@@ -0,0 +1,19 @@
+x = int("2024", 5)
+y = int("1044", 5)
+
+wynik1 = x + y
+wynik2 = x - y
+
+print(wynik1)
+print(wynik2)
+
+'''
+teraz wpisujem wynik1 i wynik2 do js
+x = wynik1
+y = wynik2
+
+x.toString(5) // 3123
+y.toString(5) // 430
+
+i mamy wyniki
+'''
